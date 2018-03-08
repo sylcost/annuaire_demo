1bf322c8b5716c438e52f6ecf80ee0b2e10b46dd
sylcost <37152750+sylcost@users.noreply.github.com>
2018-03-08 21:44:13 +0100

    Nettoyage

--- src/serveur/servNoBDD.js
+++ src/serveur/servNoBDD.js
@@ -3,28 +3,24 @@ var bodyParser = require('body-parser')
 
 
 var app = express();
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+
+// Conf pour les appels REST
+app.use(bodyParser.urlencoded({ 
   extended: false
 }));
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-// Add headers
+app.use( bodyParser.json() );
+
+// Header
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
     next();
+});
+
+var server = app.listen(5000, function() {
+    console.log('Serveur demarre sur le port 5000');
 });
 
 // Normalement stoque en BDD, compteur represente l'auto increment id.
@@ -77,9 +73,4 @@ app.delete('/supprimerContact/:id', function(req, res){
     }
    
     res.json({'message': message});
-});
-
-
-var server = app.listen(5000, function() {
-    console.log('Express server listening on port ' + 5000);
 });
